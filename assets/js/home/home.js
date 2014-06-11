b9898476af0809ad1e4020338c193ce2276e896d
JeffreyTerry <jterry94@gmail.com>
2014-06-11 16:54:52 -0500

    alpha version finished

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -8,33 +8,13 @@ function goto(url){
   window.location = url;
 }
 
-function reformatElements(){
-  if($(window).width() >= 1020){
-    $('.blank-content').css('width', '180px');
-    $('.pa-container').css('padding-left', '0');
-    $('.pa-container').css('padding-right', '0');
-  }else{
-    $('.blank-content').css('width', '0');
-    $('.pa-container').css('padding-left', '90px');
-    $('.pa-container').css('padding-right', '90px');
-  }
-}
 
 $(document).ready(function(){
   if($(window).width() >= 1020){
     $.stellar({
       horizontalScrolling: false
     });
-  }else{
-    $('.blank-content').css('width', '0');
-    $('.pa-container').css('padding-left', '90px');
-    $('.pa-container').css('padding-right', '90px');
   }
-
-
-  $(window).resize(function(){
-    reformatElements();
-  });
 });
 
 
